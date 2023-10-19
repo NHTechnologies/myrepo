1a0df933ae2d67aba07ddcb8fd50de82e8d57a8a
NTTechnologies9 <148421928+NTTechnologies9@users.noreply.github.com>
2023-10-19 10:51:07 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,50 @@
+
+
+Search tutorials & examples
+Try
+Programiz PRO
+
+Learn Python Interactively
+Python Examples
+Print Hello world!
+Add Two Numbers
+Find the Square Root
+Calculate the Area of a Triangle
+Solve Quadratic Equation
+Swap Two Variables
+Generate a Random Number
+Convert Kilometers to Miles
+
+Python Tutorials
+Python while Loop
+Python Functions
+Python break and continue
+Python User-defined Functions
+Python Basic Input and Output
+Python Numbers, Type Conversion and Mathematics
+
+
+
+Python Program to Add Two Numbers
+To understand this example, you should have the knowledge of the following Python programming topics:
+
+Python Basic Input and Output
+Python Data Types
+Python Operators
+Learn Python with Challenges
+Solve challenges and become a Python expert.
+
+
+In the program below, we've used the + operator to add two numbers.
+
+Example 1: Add Two Numbers
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
